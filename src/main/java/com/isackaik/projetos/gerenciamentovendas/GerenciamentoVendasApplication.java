99fa1f4910f3270387423f868d93bc45de71f74b
Isac Kaik <isackaik1@gmail.com>
2024-04-18 16:40:07 -0300

    add initial project

---
+++ src/main/java/com/isackaik/projetos/gerenciamentovendas/GerenciamentoVendasApplication.java
@@ -0,0 +1,13 @@
+package com.isackaik.projetos.gerenciamentovendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoVendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isackaik/projetos/gerenciamentovendas/GerenciamentoVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isackaik.projetos.gerenciamentovendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
